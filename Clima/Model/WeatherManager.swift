eefaeb584fb2d0964cd0aa3dbce93ea1cfcc4949
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-09-12 23:30:18 -0400

    Update protocol WeatherManagerDelegate to remove external parameter name

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 protocol WeatherManagerDelegate {
-    func didUpdateWeather(weather: WeatherModel)
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
 }
 
 struct WeatherManager {
@@ -39,7 +39,7 @@ struct WeatherManager {
                 
                 if let safeData = data {
                     if let weather =  self.parseJSON(weatherData: safeData) {
-                        self.delegate?.didUpdateWeather(weather: weather)
+                        self.delegate?.didUpdateWeather(self, weather: weather)
                     }
                     
 
